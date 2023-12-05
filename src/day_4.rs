7cb21bd40db2ff43fc2f4664bc463629c3aa6c63
Elijah Roussos <elijah.rou@gmail.com>
2023-12-05 19:08:04 +0200

    complete day 4

--- src/day_4.rs
+++ src/day_4.rs
@@ -2,25 +2,26 @@ use serde::{Deserialize, Serialize};
 use axum::extract::Json;
 
 #[derive(Deserialize)]
-struct Reindeer {
+pub struct Reindeer {
     name: String,
     strength: u32
 }
 
 #[derive(Deserialize)]
-struct MegaReindeer {
+pub struct MegaReindeer {
     name: String,
     strength: u32,
     speed: f64,
     height: u32,
     antler_width: u32,
     snow_magic_power: u32,
-    favourite_food: String,
-    cAnD13s_3ATeN_yesT3rdAy: String
+    favorite_food: String,
+    #[serde(rename(deserialize = "cAnD13s_3ATeN-yesT3rdAy"))]
+    candies: u32
 }
 
 #[derive(Serialize)]
-struct ContestOutcome {
+pub struct ContestOutcome {
     fastest: String,
     tallest: String,
     magician: String,
@@ -35,21 +36,27 @@ pub async fn strength(Json(reindeer): Json<Vec<Reindeer>>) -> String {
 }
 
 pub async fn contest(Json(reindeer): Json<Vec<MegaReindeer>>) -> axum::Json<ContestOutcome>{
-    let r_iter = reindeer.iter();
-
-    let tallest = r_iter.position(
-        |&x| x == r_iter
-        .map(|r| r.height)
-        .max()
-        .unwrap()
-    );
-    let wizard: u32 = 0;
-    let capitalist: u32 = 0;
+    let fastest= reindeer
+        .iter()
+        .max_by(|r1, r2| r1.speed.total_cmp(&r2.speed) )
+        .unwrap();
+    let tallest= reindeer
+        .iter()
+        .max_by(|r1, r2| r1.height.cmp(&r2.height))
+        .unwrap();
+    let wizard = reindeer
+        .iter()
+        .max_by(|r1, r2| r1.snow_magic_power.cmp(&r2.snow_magic_power))
+        .unwrap();
+    let capitalist = reindeer
+        .iter()
+        .max_by(|r1, r2| r1.candies.cmp(&r2.candies))
+        .unwrap();
     
     Json(ContestOutcome{
-        fastest: format!("Speeding past the finish line with a strength of {} is {}", ),
-        tallest: format!("{} is standing tall with his 36 cm wide antlers"),
-        magician: format!("{} could blast you away with a snow magic power of 9001"),
-        consumer: format!("{} ate lots of candies, but also some {}")
+        fastest: format!("Speeding past the finish line with a strength of {} is {}", fastest.strength, fastest.name),
+        tallest: format!("{} is standing tall with his {} cm wide antlers", tallest.name, tallest.antler_width),
+        magician: format!("{} could blast you away with a snow magic power of {}", wizard.name, wizard.snow_magic_power),
+        consumer: format!("{} ate lots of candies, but also some {}", capitalist.name, capitalist.favorite_food)
     })
 }
